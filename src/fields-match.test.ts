147a249e8517e8c56bdfc56fcc078e3f4da360d0
Sara Lissette <lissette.ibnz@gmail.com>
2019-02-13 20:35:23 +0000

    Check if the key is not belong vm

--- src/fields-match.test.ts
+++ src/fields-match.test.ts
@@ -13,21 +13,35 @@ describe('validateFieldsMatch', () => {
   it('should invalidate when null input', () => {
     // Arrange
     const viewModel = { password: null, confirmPassword: null };
-
     // Act
     const result = validateFieldsMatch(viewModel.confirmPassword, viewModel, 'password');
-
     // Assert
     expect(result.succeeded).toBeFalsy();
+  });
+
+  it('should invalidate when customParam is no belong viewmodel', () => {
+    // Arrange
+    const viewModel = { password: '1234', confirmPassword: '1234' };
+    // Act
+    const result = validateFieldsMatch(viewModel.confirmPassword, viewModel, 'wrong');
+    // Assert
+    expect(result.succeeded).toBeFalsy();
+  });
+
+  it('should invalidate when customParam is belong viewmodel', () => {
+    // Arrange
+    const viewModel = { password: '', confirmPassword: '' };
+    // Act
+    const result = validateFieldsMatch(viewModel.confirmPassword, viewModel, 'password');
+    // Assert
+    expect(result.succeeded).toBeTruthy();
   });
 
   it('should invalidate when do not match inputs', () => {
     // Arrange
     const viewModel = { password: '12345', confirmPassword: '12346' };
-
     // Act
     const result = validateFieldsMatch(viewModel.confirmPassword, viewModel, 'password');
-
     // Assert
     expect(result.succeeded).toBeFalsy();
   });
@@ -35,10 +49,8 @@ describe('validateFieldsMatch', () => {
   it('should validate when match inputs', () => {
     // Arrange
     const viewModel = { password: '12345', confirmPassword: '12345' };
-
     // Act
     const result = validateFieldsMatch(viewModel.confirmPassword, viewModel, 'password');
-
     // Assert
     expect(result.succeeded).toBeTruthy();
   });
--- src/fields-match.ts
+++ src/fields-match.ts
@@ -3,6 +3,7 @@ import { FieldValidationResult } from 'lc-form-validation';
 const defaultInvalidMessage = 'Fields do not match';
 
 const isString = (input: any) => typeof input === 'string' || input instanceof String;
+const isKeyOfVm = (key: string, vm: any) => key in vm;
 
 export const VALIDATION_TYPE = 'FIELDS-MATCH';
 
@@ -10,7 +11,7 @@ export const validateFieldsMatch = (value: string, vm: any, key: typeof vm): Fie
   const result = new FieldValidationResult();
   let isValid = false;
 
-  if (isString(value) && isString(vm[key])) {
+  if (isString(value) && isKeyOfVm(key, vm) && isString(vm[key])) {
     isValid = value === vm[key];
   }
 
