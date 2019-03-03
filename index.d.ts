a48ec6d8e3028a1a216ef5c702d4e3dd64e493bc
Sara Lissette <lissette.ibnz@gmail.com>
2019-03-03 13:03:24 +0000

    update definitions

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,10 @@
 /**
- * TODO:
- * Validates an input value as a spanish National Identity Number (DNI)
- * @param value Input value to be interpreted as DNI
+ * Validate if two fields match.
+ * @param value Input value to match
+ * @param vm The viewmodel object (form)
+ * @param key The field path in the viewmodel to compare
  */
 import FieldValidationResult from 'lc-form-validation';
 
 export declare const VALIDATION_TYPE : string;
-export function validateFieldsMatch(value: string, vm: any, key: typeof vm): FieldValidationResult;
+export function validateFieldsMatch(value: string, vm: any, key: string): FieldValidationResult;
